2d797114880fa336118006f5d23e3797e4bcf7be
Ken Wang <kwbacklogic@gmail.com>
2023-03-16 11:45:36 -0700

    update pom

--- src/test/java/net/backlogic/persistence/client/PersistenceClientTest.java
+++ src/test/java/net/backlogic/persistence/client/PersistenceClientTest.java
@@ -2,19 +2,21 @@ package net.backlogic.persistence.client;
 
 import net.backlogic.persistence.client.classicmodel.*;
 import net.backlogic.persistence.client.handler.MockServiceHandler;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class PersistenceClientTest {
 	
 	private static PersistenceClient client;
 	private static MockServiceHandler handler;
 
-	@BeforeClass
+	@BeforeAll
 	public static void setUpBeforeClass() throws Exception {
 		handler = new MockServiceHandler("http://localhost/acct/app");
 		client = new PersistenceClient(handler);
@@ -43,49 +45,49 @@ public class PersistenceClientTest {
 		//read
 		handler.setOutput("/repositories/order/read", "[{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]},{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}]");
 		orders = repository.getOrdersByCustomer(123);
-		Assert.assertEquals("/repositories/order/read", handler.getServiceUrl());
+		assertEquals("/repositories/order/read", handler.getServiceUrl());
 //		input = handler.getServiceInput();
-//		Assert.assertEquals("{\"customerNumber\":123}", input);
-		Assert.assertEquals(true, orders.size()>0);
-		Assert.assertEquals(true, orders.get(0) instanceof Order);
+//		assertEquals("{\"customerNumber\":123}", input);
+		assertEquals(true, orders.size()>0);
+		assertEquals(true, orders.get(0) instanceof Order);
 		
 		//save
 		handler.setOutput("/repositories/order/save", "[{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]},{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}]");
 		orders = getOrderList();
 		orders = repository.save(orders);
-		Assert.assertEquals("/repositories/order/save", handler.getServiceUrl());
+		assertEquals("/repositories/order/save", handler.getServiceUrl());
 //		input = handler.getServiceInput();
-//		Assert.assertEquals("[{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]},{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}]", input);
-		Assert.assertEquals(true, orders.size()>0);
-		Assert.assertEquals(true, orders.get(0) instanceof Order);
+//		assertEquals("[{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]},{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}]", input);
+		assertEquals(true, orders.size()>0);
+		assertEquals(true, orders.get(0) instanceof Order);
 				
 		//create
 		handler.setOutput("/repositories/order/create", "{\"orderNumber\":1234,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}");
 		order = getOrder();
 		order = repository.create(order);
-		Assert.assertEquals("/repositories/order/create", handler.getServiceUrl());
+		assertEquals("/repositories/order/create", handler.getServiceUrl());
 //		input = handler.getServiceInput();
-//		Assert.assertEquals("{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}", input);
-		Assert.assertNotNull(order);
-		Assert.assertEquals((Long)1234L, order.getOrderNumber());
+//		assertEquals("{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}", input);
+		assertNotNull(order);
+		assertEquals((Long)1234L, order.getOrderNumber());
 		
 		//update
 		handler.setOutput("/repositories/order/update", "{\"orderNumber\":1234,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}");
 		order = getOrder();
 		order = repository.update(order);
-		Assert.assertEquals("/repositories/order/update", handler.getServiceUrl());
+		assertEquals("/repositories/order/update", handler.getServiceUrl());
 //		input = handler.getServiceInput();
-//		Assert.assertEquals("{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}", input);
-		Assert.assertNotNull(order);
-		Assert.assertEquals((Long)1234L, order.getOrderNumber());	
+//		assertEquals("{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}", input);
+		assertNotNull(order);
+		assertEquals((Long)1234L, order.getOrderNumber());	
 		
 		//delete
 		handler.setOutput("/repositories/order/delete", "{}");
 		order = getOrder();
 		repository.delete(order);
-		Assert.assertEquals("/repositories/order/delete", handler.getServiceUrl());
+		assertEquals("/repositories/order/delete", handler.getServiceUrl());
 //		input = handler.getServiceInput();
-//		Assert.assertEquals("{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}", input);
+//		assertEquals("{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}", input);
 
 	}
 		
@@ -98,12 +100,12 @@ public class PersistenceClientTest {
 		//get product lines
 		handler.setOutput("/queries/getProductLines", "[{\"productLine\":null,\"textDescription\":null,\"htmlDescription\":null,\"image\":null,\"products\":[{\"productCode\":null,\"productName\":null,\"productLine\":null,\"productScale\":null,\"productVendor\":null,\"productDescription\":null,\"quantityInStock\":null,\"buyPrice\":null,\"msrp\":null}]},{\"productLine\":null,\"textDescription\":null,\"htmlDescription\":null,\"image\":null,\"products\":[{\"productCode\":null,\"productName\":null,\"productLine\":null,\"productScale\":null,\"productVendor\":null,\"productDescription\":null,\"quantityInStock\":null,\"buyPrice\":null,\"msrp\":null}]}]");
 		productLines = query.getProductLines();
-		Assert.assertEquals("/queries/getProductLines", handler.getServiceUrl());
+		assertEquals("/queries/getProductLines", handler.getServiceUrl());
 //		input = handler.getServiceInput();
-//		Assert.assertEquals("{}", input);
-		Assert.assertEquals(true, productLines.size()>0 );
-		Assert.assertEquals(true, productLines.get(0) instanceof ProductLine);		
-		Assert.assertEquals(true, productLines.get(0).getProducts().get(0) instanceof Product);		
+//		assertEquals("{}", input);
+		assertEquals(true, productLines.size()>0 );
+		assertEquals(true, productLines.get(0) instanceof ProductLine);		
+		assertEquals(true, productLines.get(0).getProducts().get(0) instanceof Product);		
 	}
 
 	@Test
@@ -114,13 +116,13 @@ public class PersistenceClientTest {
 		//get product lines
 		handler.setOutput("/queries/getCustomer", "{\"customerNumber\":null,\"customerName\":null,\"contactLastName\":null,\"contactFirstName\":null,\"phone\":null,\"addressLine1\":null,\"addressLine2\":null,\"city\":null,\"state\":null,\"postalCode\":null,\"country\":null,\"creditLimit\":null,\"orders\":[{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]},{\"orderNumber\":null,\"orderDate\":null,\"requiredDate\":null,\"shippedDate\":null,\"status\":null,\"comments\":null,\"customerNumber\":null,\"customerName\":null,\"orderDetails\":[{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null},{\"orderNumber\":null,\"productCode\":null,\"productName\":null,\"quantityOrdered\":null,\"priceEach\":null,\"orderLineNumber\":null}]}],\"payments\":[{\"customerNumber\":null,\"customerName\":null,\"checkNumber\":null,\"paymentDate\":null,\"amount\":null}]}");
 		customer = query.getCustomer(1234);
-		Assert.assertEquals("/queries/getCustomer", handler.getServiceUrl());
+		assertEquals("/queries/getCustomer", handler.getServiceUrl());
 //		input = handler.getServiceInput();
-//		Assert.assertEquals("{\"customerNumber\":1234}", input);
-		Assert.assertEquals(true, customer.getOrders().size()>0 );
-		Assert.assertEquals(true, customer.getOrders().get(0) instanceof Order);
-		Assert.assertEquals(true, customer.getPayments().size()>0 );
-		Assert.assertEquals(true, customer.getPayments().get(0) instanceof Payment);
+//		assertEquals("{\"customerNumber\":1234}", input);
+		assertEquals(true, customer.getOrders().size()>0 );
+		assertEquals(true, customer.getOrders().get(0) instanceof Order);
+		assertEquals(true, customer.getPayments().size()>0 );
+		assertEquals(true, customer.getPayments().get(0) instanceof Payment);
 	}
 
 	@Test
@@ -131,9 +133,9 @@ public class PersistenceClientTest {
 		//get product lines
 		handler.setOutput("/commands/duplicateProductLine", "{\"productLine\":null,\"textDescription\":null,\"htmlDescription\":null,\"image\":null,\"products\":[{\"productCode\":null,\"productName\":null,\"productLine\":null,\"productScale\":null,\"productVendor\":null,\"productDescription\":null,\"quantityInStock\":null,\"buyPrice\":null,\"msrp\":null}]},{\"productLine\":null,\"textDescription\":null,\"htmlDescription\":null,\"image\":null,\"products\":[{\"productCode\":null,\"productName\":null,\"productLine\":null,\"productScale\":null,\"productVendor\":null,\"productDescription\":null,\"quantityInStock\":null,\"buyPrice\":null,\"msrp\":null}]}");
 		command.duplicateProductLine("Car", "Boat");
-		Assert.assertEquals("/commands/duplicateProductLine", handler.getServiceUrl());
+		assertEquals("/commands/duplicateProductLine", handler.getServiceUrl());
 //		input = handler.getServiceInput();
-//		Assert.assertEquals("{\"sourceProductLine\":\"Car\",\"targetProductLine\":\"Boat\"}", input);
+//		assertEquals("{\"sourceProductLine\":\"Car\",\"targetProductLine\":\"Boat\"}", input);
 	}	
 	
 	@Test
@@ -144,8 +146,8 @@ public class PersistenceClientTest {
 		//get product lines
 		handler.setOutput("/commands/removeCustomer", "{}");
 		command.removeCustomer(1234);
-		Assert.assertEquals("/commands/removeCustomer", handler.getServiceUrl());
+		assertEquals("/commands/removeCustomer", handler.getServiceUrl());
 //		input = handler.getServiceInput();
-//		Assert.assertEquals("{\"customerNumber\":1234}", input);
+//		assertEquals("{\"customerNumber\":1234}", input);
 	}	
 }
--- src/test/java/net/backlogic/persistent/client/proxy/UrlUtilTest.java
+++ src/test/java/net/backlogic/persistent/client/proxy/UrlUtilTest.java
@@ -1,28 +1,28 @@
 package net.backlogic.persistent.client.proxy;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import net.backlogic.persistence.client.proxy.UrlUtil;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UrlUtilTest {
 	@Test
 	public void testFormatUrl() {
-		Assert.assertEquals("null should be empty string", "", UrlUtil.formatUrl(null) );
-		Assert.assertEquals("empty string should be empty string", "", UrlUtil.formatUrl("") );
-		Assert.assertEquals("test should be /test", "/test", UrlUtil.formatUrl("test") );
-		Assert.assertEquals("test/ should be /test", "/test", UrlUtil.formatUrl("test/") );
-		Assert.assertEquals("/test/ should be /test", "/test", UrlUtil.formatUrl("/test/") );
-		Assert.assertEquals("/module/test should be /module/test", "/module/test", UrlUtil.formatUrl("/module/test") );
-		Assert.assertEquals("/module/test/ should be /module/test", "/module/test", UrlUtil.formatUrl("/module/test/") );
+		assertEquals("null should be empty string", "", UrlUtil.formatUrl(null) );
+		assertEquals("empty string should be empty string", "", UrlUtil.formatUrl("") );
+		assertEquals("test should be /test", "/test", UrlUtil.formatUrl("test") );
+		assertEquals("test/ should be /test", "/test", UrlUtil.formatUrl("test/") );
+		assertEquals("/test/ should be /test", "/test", UrlUtil.formatUrl("/test/") );
+		assertEquals("/module/test should be /module/test", "/module/test", UrlUtil.formatUrl("/module/test") );
+		assertEquals("/module/test/ should be /module/test", "/module/test", UrlUtil.formatUrl("/module/test/") );
 	}
 
 	@Test
 	public void testGetUrl() {
-		Assert.assertEquals("null and test/ should be /test", "/test", UrlUtil.getUrl(null, "test/") );
-		Assert.assertEquals("empty string and test/ should be /test", "/test", UrlUtil.getUrl("", "test/") );
-		Assert.assertEquals("module and test/ should be /module/test", "/module/test", UrlUtil.getUrl("module", "test/") );
-		Assert.assertEquals("/module/ and /test/ should be /module/test", "/module/test", UrlUtil.getUrl("/module/", "/test/") );
+		assertEquals("null and test/ should be /test", "/test", UrlUtil.getUrl(null, "test/") );
+		assertEquals("empty string and test/ should be /test", "/test", UrlUtil.getUrl("", "test/") );
+		assertEquals("module and test/ should be /module/test", "/module/test", UrlUtil.getUrl("module", "test/") );
+		assertEquals("/module/ and /test/ should be /module/test", "/module/test", UrlUtil.getUrl("/module/", "/test/") );
 	}
 	
 	
